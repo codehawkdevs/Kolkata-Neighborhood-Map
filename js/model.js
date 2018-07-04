222a04e40f5de590813ba6b47fb0707e901b2191
Subhadeep Dey <contact.sdey@gmail.com>
2018-07-05 03:30:38 +0530

    Refactor to follow Airbnb JS Style Guidelines

--- js/model.js
+++ js/model.js
@@ -12,53 +12,53 @@ const locations = [{
     "lat": 22.544808,
     "lng": 88.342558,
     "wikiArticle": "Victoria_Memorial,_Kolkata",
-    "icon": "https://png.icons8.com/ultraviolet/48/000000/monument.png"
+    "icon": "https://png.icons8.com/ultraviolet/48/000000/monument.png",
 }, {
     "name": "Kalighat Kali Temple",
     "lat": 22.520244,
     "lng": 88.342087,
     "wikiArticle": "Kalighat_Kali_Temple",
-    "icon": "https://png.icons8.com/ios/40/000000/pranava-filled.png"
+    "icon": "https://png.icons8.com/ios/40/000000/pranava-filled.png",
 }, {
     "name": "Eden Gardens",
     "lat": 22.564608,
     "lng": 88.343265,
     "wikiArticle": "Eden_Gardens",
-    "icon": "https://png.icons8.com/dusk/40/000000/performance.png"
+    "icon": "https://png.icons8.com/dusk/40/000000/performance.png",
 }, {
     "name": "Alipore Zoological Garden",
     "lat": 22.537011,
     "lng": 88.33167,
     "wikiArticle": "Zoological_Garden,_Alipore",
-    "icon": "https://png.icons8.com/color/40/000000/year-of-tiger.png"
+    "icon": "https://png.icons8.com/color/40/000000/year-of-tiger.png",
 }, {
     "name": "Indian Museum",
     "lat": 22.557886 ,
     "lng": 88.351127,
     "wikiArticle": "Indian_Museum,_Kolkata",
-    "icon": "https://png.icons8.com/color/48/000000/museum.png"
+    "icon": "https://png.icons8.com/color/48/000000/museum.png",
 }, {
     "name": "Belur Math",
     "lat": 22.632185,
     "lng": 88.355891,
     "wikiArticle": "Belur_Math",
-    "icon": "https://png.icons8.com/color/50/000000/city-church.png"
+    "icon": "https://png.icons8.com/color/50/000000/city-church.png",
 }, {
     "name": "Science City",
     "lat": 22.539056,
     "lng": 88.395825,
     "wikiArticle": "Science_City_Kolkata",
-    "icon": "https://png.icons8.com/color/50/000000/physics.png"
+    "icon": "https://png.icons8.com/color/50/000000/physics.png",
 }, {
     "name": "Nicco Park",
     "lat": 22.570749,
     "lng": 88.422697,
     "wikiArticle": "Nicco_Park",
-    "icon": "https://png.icons8.com/color/50/000000/water-park.png"
+    "icon": "https://png.icons8.com/color/50/000000/water-park.png",
 }, {
     "name": "Birla Planetarium",
     "lat": 22.545534,
     "lng": 88.347382,
     "wikiArticle": "Birla_Planetarium,_Kolkata",
-    "icon": "https://png.icons8.com/color/50/000000/planet.png"
+    "icon": "https://png.icons8.com/color/50/000000/planet.png",
 }];
