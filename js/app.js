87583468566688042e30c88c44730c263d442a99
Subhadeep Dey <contact.sdey@gmail.com>
2018-07-04 20:12:44 +0530

    Optimize code for readability

--- js/app.js
+++ js/app.js
@@ -148,6 +148,7 @@ let viewModel = function() {
 
         location.marker = marker;
 
+        // Handling of "click" event on a specific marker.
         marker.addListener("click", function() {
 
             // Let the marker bounce when it is clicked.
@@ -166,17 +167,21 @@ let viewModel = function() {
     // Render content in the infowindow.
     self.wikiInfoWindow = function(location, marker, infoWindow) {
         infoWindow.marker = marker;
-        let imageData, infoWindowHead, infoWindowBody, infoWindowFooter;
+        let jsonUrl, imageData, infoWindowHead, infoWindowBody, infoWindowFooter;
 
-        let jsonUrl = "https://en.wikipedia.org/w/api.php?action=query&origin=*&prop=pageimages&format=json&piprop=original&titles=" + location.wikiArticle;
+        jsonUrl = "https://en.wikipedia.org/w/api.php?action=query&origin=*&prop=pageimages&format=json&piprop=original&titles=" + location.wikiArticle;
 
         // Make an asynchronous request to get featured image of the concerned article from Wikipedia.
         $.getJSON(jsonUrl, function(data){
+
+            // Get the link to the featured image and store it in the variable `imageData`.
             for (let pageId in data.query.pages) {
-                // Get the link to the featured image.
                 imageData = data.query.pages[pageId].original.source;
             }
 
+            /* In the following statements, `location.url` and `location.extract` are extracted from the function `getWikiData()`.
+             * They are essentially being used to get the URL and the first paragraph of the article, respectively.
+             */
             infoWindowHead = "<div><h1><a target='_blank' href='" + location.url + "'>" + marker.title + "</a></h1></div>";
             infoWindowBody = "<div> <img src='" + imageData + "' class='img-responsive' style='width: 100%; height: 270px;'><p>" + location.extract[0] + "</p>";
             infoWindowFooter = "<hr><p>Brought to you by <img src='https://png.icons8.com/windows/15/000000/wikipedia.png'> Wikipedia. Explore more on <a target='_blank' href='https://www.google.com/search?q=" + marker.title + "'>Google</a>." + "</div>";
@@ -190,12 +195,16 @@ let viewModel = function() {
     };
 
 
-    // Open the infowindow when a location is clicked from the list.
+    // Open the infowindow when a specific location from the list  is clicked.
     self.wikiInfo = function(location) {
+
+        // Render information received from Wikipedia on the infowindow.
         self.wikiInfoWindow(location, location.marker, largeInfoWindow);
 
         // Let the marker bounce when the location is selected from the list.
         location.marker.setAnimation(google.maps.Animation.BOUNCE);
+
+        // Stop the bouncing of marker after 800 ms.
         setTimeout(function() {
             location.marker.setAnimation(null);
         }, 800);
@@ -209,12 +218,12 @@ let viewModel = function() {
 
         return ko.utils.arrayFilter(self.locations(), function(location) {
 
-            //If the location is found.
+            // If the location is found, then show it.
             if (location.name.toLowerCase().indexOf(text) !== -1) {
                 location.marker.setVisible(true);
                 return true;
             }
-            // If the location isn't found.
+            // If the location isn't found, then hide it.
             else {
                 location.marker.setVisible(false);
                 return false;
@@ -224,11 +233,11 @@ let viewModel = function() {
 };
 
 
-// Fetch Weather and show modal.
-$(window).on("load", function() {
+// Fetch current weather data and show modal.
+window.addEventListener("load", function() {
 
     // Fetch the current weather data from OpenWeatherMap and append it to the <div> with the id `temperature`.
-    let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather?q=Kolkata,IN&appid=9394141b4a982828532b32f51ea24531";
+    const apiEndpoint = "https://api.openweathermap.org/data/2.5/weather?q=Kolkata,IN&appid=9394141b4a982828532b32f51ea24531";
 
     $.getJSON(apiEndpoint, function(data){
         let icon = data.weather[0].icon;
