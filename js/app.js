4c505793fddf23280d60edceda3467ccd3ce1ef2
Subhadeep Dey <contact.sdey@gmail.com>
2018-07-05 00:49:20 +0530

    Refactor code

--- js/app.js
+++ js/app.js
@@ -5,6 +5,24 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+
+// Fetch current weather data and show modal.
+window.addEventListener("load", () => {
+
+    // Fetch the current weather data from OpenWeatherMap and append it to the <div> with the id `temperature`.
+    const apiEndpoint = "https://api.openweathermap.org/data/2.5/weather?q=Kolkata,IN&appid=9394141b4a982828532b32f51ea24531";
+
+    $.getJSON(apiEndpoint, (data) => {
+        let icon = data.weather[0].icon;
+        let img = `<img src='https://openweathermap.org/img/w/${icon}.png' class='img-responsive pull-left'>`;
+        $(".temperature").append(img, data.main.temp-273.15 + "&#176;C | " + data.weather[0].main);
+    });
+
+    // Load the modal when the page loads.
+    $("#welcome-modal").modal("show");
+});
+
+
 let map;
 
 /* Initialize the map with custom styles.
@@ -102,14 +120,14 @@ function getWikiData(location) {
         window.alert("Something went wrong. Please try again later.");
     }, 8000);
 
-    let wikiUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + location.wikiArticle + "&format=json&callback=wikiCallback;";
+    let wikiUrl = `https://en.wikipedia.org/w/api.php?action=opensearch&search=${location.wikiArticle}&format=json&callback=wikiCallback`;
 
     $.ajax({
         url: wikiUrl,
         dataType: "jsonp",
         success: (response) => {
             let articleList = response[1];
-            let url = "https://en.wikipedia.org/wiki/" + articleList[0];
+            let url = `https://en.wikipedia.org/wiki/${articleList[0]}`;
             location.url = url;
             location.extract = response[2];
 
@@ -121,15 +139,14 @@ function getWikiData(location) {
 
 // ViewModel
 let viewModel = function() {
-    let self = this;
 
     /* The variable `locations` is defined in the file `model.js` which is
      * located inside the directory `js` in the root directory.
      */
-    self.locations = ko.observableArray(locations);
+    this.locations = ko.observableArray(locations);
     let largeInfoWindow = new google.maps.InfoWindow();
 
-    for (let location of self.locations()) {
+    for (let location of this.locations()) {
 
         // Get Wikipedia data for the current location.
         getWikiData(location);
@@ -155,21 +172,19 @@ let viewModel = function() {
             marker.setAnimation(google.maps.Animation.BOUNCE);
 
             // Show the infowindow when the marker is clicked.
-            self.wikiInfoWindow(location, marker, largeInfoWindow);
+            this.wikiInfoWindow(location, marker, largeInfoWindow);
 
             // Stop the animation after 800 ms.
-            setTimeout(() => {
-                location.marker.setAnimation(null);
-            }, 800);
+            setTimeout(() => location.marker.setAnimation(null), 800);
         });
     }
 
     // Render content in the infowindow.
-    self.wikiInfoWindow = (location, marker, infoWindow) => {
+    this.wikiInfoWindow = (location, marker, infoWindow) => {
         infoWindow.marker = marker;
         let jsonUrl, imageData, infoWindowHead, infoWindowBody, infoWindowFooter;
 
-        jsonUrl = "https://en.wikipedia.org/w/api.php?action=query&origin=*&prop=pageimages&format=json&piprop=original&titles=" + location.wikiArticle;
+        jsonUrl = `https://en.wikipedia.org/w/api.php?action=query&origin=*&prop=pageimages&format=json&piprop=original&titles=${location.wikiArticle}`;
 
         // Make an asynchronous request to get featured image of the concerned article from Wikipedia.
         $.getJSON(jsonUrl, (data) => {
@@ -182,9 +197,13 @@ let viewModel = function() {
             /* In the following statements, `location.url` and `location.extract` are extracted from the function `getWikiData()`.
              * They are essentially being used to get the URL and the first paragraph of the article, respectively.
              */
-            infoWindowHead = "<div><h1><a target='_blank' href='" + location.url + "'>" + marker.title + "</a></h1></div>";
-            infoWindowBody = "<div> <img src='" + imageData + "' class='img-responsive' style='width: 100%; height: 270px;'><p>" + location.extract[0] + "</p>";
-            infoWindowFooter = "<hr><p>Brought to you by <img src='https://png.icons8.com/windows/15/000000/wikipedia.png'> Wikipedia. Explore more on <a target='_blank' href='https://www.google.com/search?q=" + marker.title + "'>Google</a>." + "</div>";
+            infoWindowHead = `<div>
+                                <h1><a target='_blank' href='${location.url}'>${marker.title}</a></h1>
+                             </div>`;
+            infoWindowBody = `<div>
+                                <img src='${imageData}' class='img-responsive' style='width: 100%; height: 270px;'><p>${location.extract[0]}</p>`;
+            infoWindowFooter = `<hr><p>Brought to you by <img src='https://png.icons8.com/windows/15/000000/wikipedia.png'> Wikipedia. Explore more on <a target='_blank' href='https://www.google.com/search?q=${marker.title}'>Google</a>.
+                             </div>`;
 
             // Set content in the infowindow.
             infoWindow.setContent(infoWindowHead + infoWindowBody + infoWindowFooter);
@@ -196,27 +215,25 @@ let viewModel = function() {
 
 
     // Open the infowindow when a specific location from the list  is clicked.
-    self.wikiInfo = (location) => {
+    this.wikiInfo = (location) => {
 
         // Render information received from Wikipedia on the infowindow.
-        self.wikiInfoWindow(location, location.marker, largeInfoWindow);
+        this.wikiInfoWindow(location, location.marker, largeInfoWindow);
 
         // Let the marker bounce when the location is selected from the list.
         location.marker.setAnimation(google.maps.Animation.BOUNCE);
 
         // Stop the bouncing of marker after 800 ms.
-        setTimeout(() => {
-            location.marker.setAnimation(null);
-        }, 800);
+        setTimeout(() => location.marker.setAnimation(null), 800);
     };
 
     // Search and filter available location(s).
-    self.inputLocation = ko.observable("");
+    this.inputLocation = ko.observable("");
 
-    self.locationItem = ko.computed(() => {
-        let text = self.inputLocation().toLowerCase();
+    this.locationItem = ko.computed(() => {
+        let text = this.inputLocation().toLowerCase();
 
-        return ko.utils.arrayFilter(self.locations(), (location) => {
+        return ko.utils.arrayFilter(this.locations(), (location) => {
 
             // If the location is found, then show it.
             if (location.name.toLowerCase().indexOf(text) !== -1) {
@@ -231,20 +248,3 @@ let viewModel = function() {
         });
     });
 };
-
-
-// Fetch current weather data and show modal.
-window.addEventListener("load", () => {
-
-    // Fetch the current weather data from OpenWeatherMap and append it to the <div> with the id `temperature`.
-    const apiEndpoint = "https://api.openweathermap.org/data/2.5/weather?q=Kolkata,IN&appid=9394141b4a982828532b32f51ea24531";
-
-    $.getJSON(apiEndpoint, (data) => {
-        let icon = data.weather[0].icon;
-        let img = "<img src='https://openweathermap.org/img/w/" + icon + ".png' class='img-responsive pull-left'>";
-        $(".temperature").append(img, data.main.temp-273.15 + "&#176;C | " + data.weather[0].main);
-    });
-
-    // Load the modal when the page loads.
-    $("#myModal").modal("show");
-});
